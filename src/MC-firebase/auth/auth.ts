37614475c6e90134dd87b3bc79b6500909bf3bf9
Magner A. Lesmes <magles978@gmail.com>
2020-06-09 14:14:39 -0500

    armando clase auth con proveedores de autenticacion externos

--- src/MC-firebase/auth/auth.ts
+++ src/MC-firebase/auth/auth.ts
@@ -12,6 +12,8 @@ export class Auth {
     private UserCredential:firebase.auth.UserCredential;
     public static currentUser:firebase.User;
 
+    private mapAuthProviders:Map<string, any>;
+
     constructor() {       
         //configurar los proveedores de autenticacion 
         //(google, facebook, twitter) para que muestren
@@ -19,6 +21,7 @@ export class Auth {
         firebase.auth().languageCode = 'es';
 
         this.AuthStateChanged();
+        this.mapAuthProviders = new Map();
     }
 
     /*AuthStateChanged()*/
@@ -43,6 +46,66 @@ export class Auth {
             })   
     }
 
-    public
+
+    /*signIn()*/
+    //Loguearse
+    //Parametros:
+    //
+    public signIn(
+        nomProvider:"local" | "google" | "facebook",
+        type: "PopUp" | "Redirect"
+    ){
+        switch (nomProvider) {
+            case "local":
+
+                break;
+
+            case "google":
+                const provider = this.mapAuthProviders.get(nomProvider);
+                
+                if (type == "PopUp") {
+                    return firebase.auth().signInWithPopup(provider)
+                    .then((credentials) => {
+                        this.UserCredential = credentials;
+                        return credentials.user;
+                    })
+                }
+                
+                if (type == "Redirect") {
+                    firebase.auth().signInWithRedirect(provider);
+                    return  firebase.auth().getRedirectResult()
+                    .then((credentials) => {
+                        this.UserCredential = credentials;
+                        return credentials.user;
+                    })
+                }
+
+                break;                
+
+            case "facebook":
+
+                break;                    
+        
+            default:
+                break;
+        }
+    }
+
+    /*createAuthByEmail()*/
+    // configura el uso de autenticacion por Google
+    private  configAuthWithGoogle(){
+        const nomProvider = "google";
+        const provider = new firebase.auth.GoogleAuthProvider();
+        //indica los campos que se desean leer del usuario (opcional)
+        provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+        //asigna propiedades personalizadas que se reciben del usuario autenticado
+        provider.setCustomParameters({
+            //..Lo que se necesite..
+        });
+        this.mapAuthProviders.set(nomProvider, provider);
+
+    }
+
+
 
 }
